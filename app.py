261aa0077be71ec5ceed0a403078e48f480bf7c8
Burla Sai Teja <66131330+BurlaSaiTeja@users.noreply.github.com>
2022-03-23 23:42:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,34 +8,14 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
-def forecast(n):
-    df = readData()
-    best_model = joblib.load('timeSeries.pkl')
-    forecast_values = best_model.get_forecast(steps = n)
-    forecast_ci = forecast_values.conf_int()
-    print(df['Order_Date'],df['Sales'])
-    ax = plt.plot(df['Order_Date'], df['Sales'], figsize = (16, 6), legend = True, color='purple')
-    forecast_values.predicted_mean.plot(ax=ax, label='Forecast', figsize = (16, 6), grid=True)
-    ax.fill_between(forecast_ci.index,
-                    forecast_ci.iloc[: , 0],
-                    forecast_ci.iloc[: , 1], color='pink', alpha = .5)
-    plt.title('Sales Forecast', size = 16)
-    plt.ylabel('Sales', size=12)
-    plt.legend(loc='upper left', prop={'size': 12})
-    ax.axes.get_xaxis().set_visible(True)
-    ax.text(540, 400, 'Forecasted Values', fontsize=12,  color='black')
-    plt.savefig('static/forecast1.png')
-    #plt.show()
-    return True
 
-#@app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods = ["GET", "POST"])
 def hello():
     if request.method == "POST":
-        '''df = readData()
-        best_model = joblib.load('timeSeries.pkl')'''
-        #forecast(int(request.form.get('steps')))
-        #forecast_values = best_model.get_forecast(steps=int(request.form.get('steps')))
-        '''df.plot(x='Order_Date', y='Sales', figsize=(16, 6), legend=True, color='purple')
+        df = readData()
+        best_model = joblib.load('timeSeries.pkl')
+        forecast_values = best_model.get_forecast(steps=int(request.form.get('steps')))
+        df.plot(x='Order_Date', y='Sales', figsize=(16, 6), legend=True, color='purple')
         plt.title('Sales', size=16)
         plt.ylabel('Sales', size=12)
         plt.legend(loc='upper left', prop={'size': 12})
@@ -45,7 +25,7 @@ def hello():
         plt.title('Forecast', size=16)
         plt.ylabel('Sales', size=12)
         plt.legend(loc='upper left', prop={'size': 12})
-        plt.savefig('static/Forecast.png')'''
+        plt.savefig('static/Forecast.png')
     return render_template('index.html')
 
 def readData():
@@ -53,5 +33,4 @@ def readData():
     return df
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    forecast(70)
+    app.run(debug=True)
